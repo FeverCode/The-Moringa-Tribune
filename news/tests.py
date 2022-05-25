87264f6940191ad7a41ea8e195e064d178703d57
FeverCode <giddylancs.gl@gmail.com>
2022-05-25 07:45:25 +0300

    Created test models

--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,20 @@
+import email
 from django.test import TestCase
+from .models import Editor,Article,tags
 
 # Create your tests here.
+class EditorTestCase(TestCase):
+    
+    # Set up method
+    def setUp(self):
+        self.giddy= Editor(first_name = 'Giddy', last_name = 'Lancs', email = 'giddy@mail.com')
+        
+#Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.giddy,Editor))
+        
+    # Testing Save Method
+    def test_save_method(self):
+        self.giddy.save_editor()
+        editors = Editor.objects.all()
+        self.assertTrue(len(editors) > 0)
