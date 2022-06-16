de700ed5816594e632dc58c5e7e942920e0ccfe1
FeverCode <giddylancs.gl@gmail.com>
2022-06-16 14:31:58 +0300

    updated debug settings

--- tribune/settings.py
+++ tribune/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'tribune.wsgi.application'
 
 MODE = config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG')
 # development
 if config('MODE') == "dev":
    DATABASES = {
